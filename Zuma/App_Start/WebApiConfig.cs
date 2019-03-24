cde18c013d0a1399d90c23cce3295f2eac77f44e
Virus1902 <michal@msit.pl>
2019-03-24 22:08:00 +0100

    Add Jwt authorization

--- Zuma/App_Start/WebApiConfig.cs
+++ Zuma/App_Start/WebApiConfig.cs
@@ -14,6 +14,8 @@ namespace Zuma
             // Trasy składnika Web API
             config.MapHttpAttributeRoutes();
 
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ Zuma/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Zuma.Controllers
+{
+    public class AuthController : ApiController
+    {
+        // GET: Auth
+        public string Index()
+        {
+            return "Auth";
+        }
+    }
+}
